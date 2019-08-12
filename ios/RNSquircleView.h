970406b50d15dc24340f83c160207ed5ec48e712
holgersindbaek <holgersindbaek@gmail.com>
2019-08-12 14:33:18 +0200

    save

--- ios/RNSquircleView.h
+++ ios/RNSquircleView.h
@@ -1,11 +1,7 @@
 #ifndef SquircleView_h
 #define SquircleView_h
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
 
 #import "UIKit/UIKit.h"
 
